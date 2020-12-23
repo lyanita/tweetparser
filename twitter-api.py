33a9c04438f3bf5864b86744bd6ed79ba090fa33
Anita <anitaly.ubc@gmail.co>
2020-12-23 00:24:49 -0500

    Reorder main dataframe

--- twitter-api.py
+++ twitter-api.py
@@ -57,7 +57,7 @@ class TwitterClient(object):
         """Classify sentiment of passed tweet using textblob's sentiment method"""
         #Create TextBlob object of passed tweet text
         analysis = TextBlob(self.clean_tweet(tweet))
-        #Set sentiment
+        #Set sentiment based on polarity score
         if analysis.sentiment.polarity > 0:
             return 'positive'
         elif analysis.sentiment.polarity == 0:
@@ -71,20 +71,25 @@ class TwitterClient(object):
         tweets = []
         try:
             #Call twitter api to fetch tweets
-            fetched_tweets = self.api.search(q=query, geocode=geo, count=count, result_type=result_type, lang="en")
+            fetched_tweets = self.api.search(q=query, geocode=geo, count=count, result_type=result_type, lang="en", tweet_mode="extended")
             #Parsing tweets one by one
             for tweet in fetched_tweets:
                 #Empty dictionary to store required parameters of a tweet
                 parsed_tweet = {}
-                #Saving text description of tweet
-                parsed_tweet['text'] = tweet.text
+                #Saving data of tweet
+                parsed_tweet['text'] = tweet.full_text
                 parsed_tweet['date'] = tweet.created_at
                 parsed_tweet['user'] = tweet.user.screen_name
-                parsed_tweet['location'] = tweet.user.location
+                parsed_tweet['name'] = tweet.user.name
+                parsed_tweet['id'] = tweet.user.id_str
+                parsed_tweet['url'] = tweet.user.url
+                parsed_tweet['user_defined_location'] = tweet.user.location
+                parsed_tweet['coordinates'] = tweet.coordinates
                 parsed_tweet['friends'] = tweet.user.friends_count
-                #Saving sentiment of tweet
-                parsed_tweet['polarity'] = self.get_tweet_polarity(tweet.text)
-                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+                parsed_tweet['followers'] = tweet.user.followers_count
+                #Saving sentiment/polarity of tweet
+                parsed_tweet['polarity'] = self.get_tweet_polarity(tweet.full_text)
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.full_text)
                 #Appending parsed tweet to tweets list
                 if tweet.retweet_count > 0:
                     #If tweet has retweets, ensure that it is appended only once
@@ -102,7 +107,7 @@ class TwitterClient(object):
         """Return most popular and recent trends"""
         trends = []
         try:
-            fetched_trends = self.api.trends_place(woeid) #Canada 23424803
+            fetched_trends = self.api.trends_place(woeid) #Canada: 23424803
             for trend in fetched_trends[0]["trends"]:
                 parsed_trend = {}
                 parsed_trend["name"] = trend["name"]
@@ -131,20 +136,19 @@ class TwitterClient(object):
         return fetched_tweets
 
 def main():
-    st.title('Tweet Parser App')
     #Creating object of TwitterClient Class
     api = TwitterClient()
     #Calling function to get tweets, users and trends
     print("This program fetches a number of tweets based on an entered query, analyzes the sentiment of those tweets using the TextBlob training dataset and pulls the top trends based on geography\n\n")
-    st.subheader("This program fetches a number of tweets based on an entered query, analyzes the sentiment of those tweets using the TextBlob training dataset and pulls the top trends based on geography")
     #count = int(input("Enter the number of search results to fetch: "))
-    count = int(st.sidebar.slider('Select a number of tweets to parse', 1, 500, 100))
+    count = int(st.sidebar.slider('Select a number of tweets to parse', 1, 500, 500))
     #keyword = input("Enter a keyword: ")
-    keyword = st.sidebar.text_input("Enter a keyword to search in Twitter", "trump")
+    keyword = st.sidebar.text_input("Enter a keyword to search in Twitter", "winter")
     #result_type = input("Enter a result type (mixed, recent, popular): ")
-    result_type = st.sidebar.selectbox('Choose the result type to receive', ('mixed', 'recent', 'popular'))
+    result_type = st.sidebar.selectbox('Choose the result type to receive', ('recent', 'popular', 'mixed'))
     locator = gp.Nominatim(user_agent="myGeocoder")
     place = st.sidebar.selectbox('Choose a location to filter the results by', ('Toronto, Ontario, Canada', 'Vancouver, British Columbia, Canada', 'Montreal, Quebec, Canada'))
+    #location = gp.RateLimiter(locator.geocode(place), min_delay_seconds=1)
     location = locator.geocode(place)
     radius = str(st.sidebar.slider('Select a radius span (in km) for the search', 1, 10000, 1000))
     geo = str(location.latitude) + "," + str(location.longitude) + "," + radius +"km"
@@ -157,7 +161,9 @@ def main():
     elif place == "Montreal, Quebec, Canada":
         woeid = 3534
     trends = api.get_trends(woeid)
-    json = api.get_json(query=keyword, geo=geo, count=count, result_type=result_type)
+
+    #View JSON for Tweet
+    #json = api.get_json(query=keyword, geo=geo, count=count, result_type=result_type)
     #print(json)
 
     #Printing user details of fetched tweets
@@ -211,47 +217,56 @@ def main():
     for trend in trends:
         print(trend["name"] + ": " + str(trend["tweet_volume"]))
 
+    #places = tweet_df["location"].apply(lambda x: GeoText(x).cities)
+    #cities = list(places)
+    #cleanse = []
+    #clean = ["0" if x == [] else x for x in cities]
+    #for city in clean:
+    #    place = city[0]
+    #    cleanse.append(place)
+    #tweet_df["city"] = cleanse
+
+    st.title('Twitter Parser Web App')
+    st.subheader("Interested in scraping data from Twitter? Use the parameters in the sidebar to continue!")
+    st.text("Twitter Search Data")
     tweet_df = pd.DataFrame(tweets)
 
-    places = tweet_df["location"].apply(lambda x: GeoText(x).cities)
-    cities = list(places)
-    cleanse = []
-    clean = ["0" if x == [] else x for x in cities]
-    for city in clean:
-        place = city[0]
-        cleanse.append(place)
-    tweet_df["city"] = cleanse
     geolocator = gp.Nominatim(user_agent="myGeocoder",timeout=2)
-    tweet_df["gcode"] = tweet_df.location.apply(geolocator.geocode, language="en")
-    tweet_df["latitude"] = [0 if g is None else g.latitude for g in tweet_df.gcode]
-    tweet_df["longitude"] = [0 if g is None else g.longitude for g in tweet_df.gcode]
-    tweet_df = tweet_df[tweet_df.gcode.notnull()]
-    tweet_df["gcode"] = tweet_df.gcode.astype(str)
-    tweet_df = tweet_df[tweet_df['gcode'].str.endswith("Canada")]
+    tweet_df["location"] = tweet_df.user_defined_location.apply(geolocator.geocode, language="en")
+    tweet_df["latitude"] = [0 if g is None else g.latitude for g in tweet_df.location]
+    tweet_df["longitude"] = [0 if g is None else g.longitude for g in tweet_df.location]
+    tweet_df = tweet_df[tweet_df.location.notnull()]
+    tweet_df["location"] = tweet_df.location.astype(str)
+    tweet_df = tweet_df[tweet_df['location'].str.endswith("Canada")]
     midpoint = (np.average(tweet_df['latitude']), np.average(tweet_df['longitude']))
     #st.deck_gl_chart(viewport={'latitude': midpoint[0], 'longitude': midpoint[1], 'zoom': 4}, layers=[{'type': 'ScatterplotLayer', 'data': tweet_df["city", "latitude", "longitude"], 'radiusScale': 250, 'radiusMinPixels': 5, 'getFillColor': [248, 24, 148],}])
-
-    token = "sk.eyJ1IjoibHlhbml0YSIsImEiOiJja2l6aGI1aHUwZWVoMzB0ZDF6d2c3enllIn0.jdVPFoJVzrm92PUY405RxA"
     st.map(tweet_df[["latitude", "longitude"]])
 
-    st.dataframe(tweet_df)
-    csv = tweet_df.to_csv(index=False)
+    #Mapbox Token
+    token = "sk.eyJ1IjoibHlhbml0YSIsImEiOiJja2l6aGI1aHUwZWVoMzB0ZDF6d2c3enllIn0.jdVPFoJVzrm92PUY405RxA"
+
+    column_names = ['date', 'user', 'name', 'id', 'url', 'user_defined_location', 'location', 'coordinates', 'latitude', 'longitude', 'friends', 'followers', 'text', 'sentiment', 'polarity']
+    tweet_reordered_df = tweet_df.reindex(columns=column_names)
+    st.dataframe(tweet_reordered_df)
+    csv = tweet_reordered_df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download = tweet_data.csv>Download CSV File</a> (click and save as &lt;filename&gt;.csv)'
     st.markdown(href, unsafe_allow_html=True)
-    polarity_df = tweet_df[["location", "polarity"]]
-    polarity_chart = alt.Chart(polarity_df).mark_bar().encode(x="polarity", y=alt.Y("location", sort="x"),
-                                                              color=alt.Color("location", legend=None)).interactive()
+
+    #polarity_df = tweet_df[["location", "polarity"]]
+    #polarity_chart = alt.Chart(polarity_df).mark_bar().encode(x="polarity", y=alt.Y("location", sort="x"), color=alt.Color("location", legend=None)).interactive()
     #st.altair_chart(polarity_chart)
 
     sentiment_list = ["Positive", "Negative", "Neutral"]
-    sentiment_pct = [positive, negative, neutral]
-    sentiment_dt = {'sentiment': sentiment_list, 'percent': sentiment_pct}
+    sentiment_percentage = [positive, negative, neutral]
+    sentiment_dt = {'sentiment': sentiment_list, 'percent': sentiment_percentage}
     sentiment_df = pd.DataFrame(sentiment_dt)
     st.dataframe(sentiment_df)
 
-    st.text("Top Twitter Trends for Specified Location")
+    st.text("")
+    st.text("Top Twitter Trends")
     trends_df = pd.DataFrame(trends)
+    trends_df = trends_df[trends_df.tweet_volume.notnull()]
     st.dataframe(trends_df)
 
 if __name__ == "__main__":
